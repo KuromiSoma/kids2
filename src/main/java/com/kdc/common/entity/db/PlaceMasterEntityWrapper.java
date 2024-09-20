1894a2709c22681de67ef1ebf782736bc5100250
KuromiSoma <tamagoyaki.zyukuzyuku@gmail.com>
2024-09-20 17:49:36 +0900

    STS4上でのエラーが全て消えたが、ログインができない。

--- src/main/java/com/kdc/common/entity/db/PlaceMasterEntityWrapper.java
+++ src/main/java/com/kdc/common/entity/db/PlaceMasterEntityWrapper.java
@@ -2,36 +2,76 @@ package com.kdc.common.entity.db;
 
 import java.math.BigDecimal;
 
-public class PlaceMasterEntityWrapper {
+import org.apache.commons.lang3.StringUtils;
 
+public class PlaceMasterEntityWrapper extends PlaceMasterEntity {
+
+	private BigDecimal latitude;
+	private BigDecimal longitude;
+	
+	/**
+	 * @return latitude
+	 */
 	public BigDecimal getLatitude() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		return latitude;
 	}
-
+	/**
+	 * @param latitude セットする latitude
+	 */
+	public void setLatitude(BigDecimal latitude) {
+		this.latitude = latitude;
+	}
+	/**
+	 * @param latitude セットする latitude
+	 */
+	public void setLatitude(String latitude) {
+		this.latitude = new BigDecimal(latitude);
+	}	
+	/**
+	 * @return longitude
+	 */
 	public BigDecimal getLongitude() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		return longitude;
 	}
-
-	public int getRadius() {
-		// TODO 自動生成されたメソッド・スタブ
-		return 0;
+	/**
+	 * @param longitude セットする longitude
+	 */
+	public void setLongitude(BigDecimal longitude) {
+		this.longitude = longitude;
 	}
-
-	public Object getPlacename() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+	/**
+	 * @param longitude セットする longitude
+	 */
+	public void setLongitude(String longitude) {
+		this.longitude = new BigDecimal(longitude);
 	}
-
-	public String getIconid() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+	/**
+	 * @return longitudeandlatitude
+	 */
+	@Deprecated
+	public String getLongitudeandlatitude() {
+		return String.format("(%s,%s)", this.latitude, this.longitude);
 	}
-
-	public Object getPlacetypeflg() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+	/**
+	 * @param latitude セットする latitude
+	 * @param longitude セットする longitude
+	 */
+	public void setLongitudeandlatitude(BigDecimal latitude, BigDecimal longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
 	}
-
+	/**
+	 * @param longitudeandlatitude セットする longitudeandlatitude
+	 */
+	@Deprecated
+	public void setLongitudeandlatitude(String longitudeandlatitude) {
+		super.setLongitudeandlatitude(longitudeandlatitude);
+		longitudeandlatitude = StringUtils.remove(longitudeandlatitude, '(');
+		longitudeandlatitude = StringUtils.remove(longitudeandlatitude, ')');
+		String temp[] = longitudeandlatitude.split(",");
+		this.latitude = new BigDecimal(temp[0]);
+		this.longitude = new BigDecimal(temp[1]);
+	}
+	
 }
+
